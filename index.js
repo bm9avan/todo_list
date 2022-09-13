ff2f83e2af807bdf737e2990cf016e1ce58f0454
bmp <mggpavan@gmail.com>
2022-09-13 19:54:23 +0530

    first and last commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var i =1;
+    function app() {
+    
+        document.getElementById('li').innerHTML=`<input type="text" id="liinput" name="in"> <button name="add" onclick=add()>add</button> <button name="dull" onclick=dull()>dull</button><br>`;
+    }
+    
+    function add() {
+        let temp = document.getElementById(`liinput`).value;
+        let need= temp;  
+        console.log(need)
+        document.getElementById(`do`).innerHTML+=`<div id="spanli${i}"><span>${need}</span>&emsp;<button name="remove" onclick=remove(${i}) id="rem${i}">remove</button></div>`; 
+        i++  
+    }
+    
+    function remove(i) {
+        document.getElementById(`spanli${i}`).innerHTML= ''; 
+    }
+    function dull(){
+        document.getElementById('li').innerHTML=''
+    }
