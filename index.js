667d79b97e57bc4e914799fbcae7d2e12ff32bee
bmp <mggpavan@gmail.com>
2022-09-14 14:28:15 +0530

    "all upgaraded using HTML set&remove attribute"

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var i =1;
     function app() {
     
         document.getElementById('li').innerHTML=`<input type="text" id="liinput" name="in"> <button name="add" onclick=add()>add</button> <button name="dull" onclick=dull()>dull</button><br>`;
+        li.removeAttribute('hidden')
     }
     
     function add() {
         let temp = document.getElementById(`liinput`).value;
         let need= temp;  
-        console.log(need)
         document.getElementById(`do`).innerHTML+=`<div id="spanli${i}"><span>${need}</span>&emsp;<button name="remove" onclick=remove(${i}) id="rem${i}">remove</button></div>`; 
         i++  
     }
@@ -18,5 +18,6 @@ var i =1;
         
     }
     function dull(){
-        document.getElementById('li').innerHTML=''
+        let li=document.getElementById(`li`)
+        li.setAttribute('hidden', 'true')
     }
